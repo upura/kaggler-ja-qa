a99dffa29ef06976a77619ec37d68c14214ee2df
upura <upura0@gmail.com>
2020-03-20 17:52:25 +0900

    fix for search prop

--- nuxt/front/pages/index.vue
+++ nuxt/front/pages/index.vue
@@ -7,41 +7,31 @@
             <v-card>
               <v-app-bar color="primary" dark>
                 <v-toolbar-title>kaggler-ja QA</v-toolbar-title>
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn color="pink" dark small absolute bottom right fab to="/create" v-on="on">+</v-btn>
-                  </template>
-                  <span>Add new QA</span>
-                </v-tooltip>
               </v-app-bar>
               <v-form ref="listForm" lazy-validation>
                 <v-container fluid class="pa-1">
                   <v-row class="px-2">
                     <v-col cols="6" class="pa-1">
                       <v-text-field
-                        clearable
-                        label="検索"
-                        name="q_text"
                         prepend-inner-icon="mdi-magnify"
-                        maxlength="64"
-                        v-model="model.q_text"
-                        @change="loadList"
+                        v-model="search"
+                        label="Search"
+                        single-line
+                        hide-details
                       ></v-text-field>
                     </v-col>
                     <v-col cols="3" class="pa-1">
                       <v-select
                         label="種別"
-                        name="label"
                         item-text="label"
                         item-value="value"
                         :items="[
                           { label: '-', value: null },
-                          { label: '前処理', value: 0 },
-                          { label: '特徴量エンジニアリング', value: 1 },
-                          { label: 'モデリング', value: 2 },
+                          { label: '前処理', value: '前処理' },
+                          { label: '特徴量エンジニアリング', value: '特徴量エンジニアリング' },
+                          { label: 'モデリング', value: 'モデリング' },
                         ]"
-                        v-model="model.label"
-                        @change="loadList"
+                        v-model="search"
                       ></v-select>
                     </v-col>
                   </v-row>
@@ -50,17 +40,8 @@
                   :headers="headers"
                   :items="items"
                   :search="search"
-                  :options.sync="options"
-                  :server-items-length="total"
-                  :footer-props="{
-                    'items-per-page-options': [10, 20, 50, 100, 200, 300, 400, 500],
-                    showFirstLastPage: true,
-                  }"
-                  :loading="loading"
-                  locale="ja-jp"
-                  loading-text="読込中"
-                  no-data-text="データがありません。"
-                  class="elevation-1"
+                  :sort-by.sync="sortBy"
+                  :sort-desc.sync="sortDesc"
                 >
                   <template v-slot:item.q_text="{ item }">
                     {{ item.q_text.slice(0, 90) + '......' }}
@@ -85,60 +66,28 @@
 import Axios from 'axios'
 export default {
   data: () => ({
-    loading: false,
     headers: [
       { text: '質問文', align: 'left', sortable: false, value: 'q_text' },
-      { text: '質問日時', align: 'left', sortable: false, value: 'q_posted_at' },
+      { text: '質問日時', align: 'left', sortable: true, value: 'q_posted_at' },
       { text: '種別', align: 'left', sortable: false, value: 'label' },
     ],
-    options: {
-      page: 1,
-      itemsPerPage: 20,
-      sortBy: ['q_posted_at'],
-      sortDesc: [false],
-    },
     items: [],
     search: '',
-    total: 0,
-    selectionItems: {
-      label: ['前処理', '特徴量エンジニアリング', 'モデリング'],
-    },
-    model: {
-      'title': '',
-      label: null,
-    },
+    sortBy: 'q_posted_at',
+    sortDesc: true,
   }),
-  watch: {
-    options: {
-      handler() {
-        this.loadList()
-      },
-      deep: true,
-    },
-  },
   methods: {
     async loadList() {
-      this.loading = true
       try {
-        const res = await this.$axios.get(
-          '/api/qa/',
-          Object.assign(this.model, {
-            offset: (this.options.page - 1) * this.options.itemsPerPage,
-            limit: this.options.itemsPerPage
-          })
-        )
-        if (res.data) {
-          this.items = res.data.results
-          this.total = res.data.count
-        }
+        const res = await this.$axios.get('/api/qa/')
+        this.items = res.data.results
       } catch (error) {
         alert('情報を取得できませんでした。時間をおいてやり直してください。')
       }
-      this.loading = false
     },
   },
-  created: function() {
+  mounted() {
     this.loadList()
-  },
+  }
 }
 </script>
